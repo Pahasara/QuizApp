0ab6b817002e3746a30dd1eef497b4870c384ab0
DvNET <46932317+Pahasara@users.noreply.github.com>
2025-02-03 23:07:36 +0530

    Add admin dashboard

---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuizCreator from '../components/QuizCreator';
+
+const AdminDashboard = () => {
+  return (
+    <div className="admin-dashboard">
+      <QuizCreator />
+    </div>
+  );
+};
+
+export default AdminDashboard;
